12b934ba0b51704a4c806570258401375e58e80c
Joris Mertz <info@jorismertz.nl>
2024-02-21 19:06:56 +0100

    fix: prevent multiple instances with pid file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use signal_hook::{consts::{SIGINT,SIGTERM}, iterator::Signals};
+use std::{error::Error, thread};
+use std::{fs, path::PathBuf, process};
 use enigo::{Enigo, KeyboardControllable};
 use mactype::{Combinations, Leader};
 use rdev::{listen, Event, EventType, Key};
@@ -19,12 +22,55 @@ impl State {
     }
 }
 
+fn get_pid_file_path() -> PathBuf {
+    // This feature is deprecated because of windows incompatibility,
+    // doesn't make sense to install a crate in this case.
+    let mut xdg_home_path = std::env::home_dir().expect("Unable to lock PID file, can't find home directory path");
+
+    xdg_home_path.push(".mactype.pid");
+    xdg_home_path
+}
+
+fn lock_pid_file(path: &PathBuf) -> std::io::Result<u32> {
+    if let Ok(_) = fs::read(&path) {
+        panic!("Unable to lock PID file, file already exists");
+    }
+    let pid = process::id();
+    fs::write(path, pid.to_string())?;
+    Ok(pid)
+}
+
+fn unlock_pid_file(path: &PathBuf) -> std::io::Result<()> {
+    fs::remove_file(path)?;
+    return Ok(())
+}
+
+fn spawn_pid_file_unlocker(path: PathBuf) -> Result<(), Box<dyn Error>> {
+    let mut signals = Signals::new(&[SIGINT,SIGTERM])?;
+
+    thread::spawn(move || {
+        for _ in signals.forever() {
+            unlock_pid_file(&path).expect("Failed to spawn pid file unlocker thread, make sure to remove it manually");
+            std::process::exit(0);
+        }
+    });
+
+    Ok(())
+}
+
+
 fn main() {
     let mut state = State {
         leader: None,
         keystrokes: Vec::new(),
         block_for: 0,
     };
+
+    let pid_file_path = get_pid_file_path();
+     lock_pid_file(&pid_file_path).unwrap();
+
+    // This will delete / 'unlock' the pid file upon receiving SIGTERM or SIGINT
+    spawn_pid_file_unlocker(pid_file_path).unwrap();
 
     if let Err(error) = listen(move |event| callback(event, &mut state)) {
         println!("Error: {:?}", error);
